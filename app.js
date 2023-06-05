a2ad98963590cc8c9b709e6fc15ab4d28c7c050b
Abimbola Adesoye <bimboadesoye44@gmail.com>
2023-06-05 13:50:38 +0100

    commit

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const PORT = 8080;
 
 // config ejs
 app.set("view engine", "ejs");
+require("dotenv").config();
+
+// Custom Middleware
+// app.use((req, res, next) => {
+//   console.log("A request was just made");
+//   console.log(req.method);
+//   console.log(req.path);
+//   next();
+// });
+
+app.use(express.static("public"));
+
+mongoose
+  .connect(process.env.DBURL)
+  .then(() => {
+    console.log("DB connected succesfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const trainees = [
   { name: "Christy", profession: "Front-end dev" },
@@ -12,6 +34,7 @@ const trainees = [
   { name: "John", profession: "Desktop dev" },
 ];
 
+// Routes
 app.get("/", (req, res) => {
   res.status(200).render("index", { title: "EJS Home Page", trainees });
 });
@@ -20,8 +43,12 @@ app.get("/about", (req, res) => {
   res.status(200).render("about", { title: "EJS About Page" });
 });
 
+app.get("/todo/create", (req, res) => {
+  res.status(200).render("createList", { title: "EJS create-todo Page" });
+});
+
 app.use((req, res) => {
-  res.status(404).render("404", { title: "EJS 404 Page" });
+  res.status(404).render("404", { title: "EJS Error" });
 });
 
 app.listen(PORT, () => {
